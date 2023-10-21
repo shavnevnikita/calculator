06953056e4d46832608e2e4546e6cc473049a0e6
shavnevnikita <shavnevnikita@gmail.com>
2023-10-21 16:22:39 +0300

    step 2

--- src.py
+++ src.py
@@ -2,7 +2,22 @@ from decimal import *
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-getcontext().prec = 4
+
+def nicify(num):
+    digits = list(str(num).rstrip('0'))
+    dot_pos = digits.index('.')
+    if digits[-1] == '.':
+        digits.pop()
+    new_digits = []
+    for i in range(dot_pos):
+        new_digits.append(digits[i])
+        if (i+1) % 3 == dot_pos % 3:
+            new_digits.append(" ")
+    if new_digits[-1] == " ":
+        new_digits.pop()
+    for i in range(dot_pos, len(digits)):
+        new_digits.append(digits[i])
+    return "".join(new_digits)
 
 @app.route("/", methods=["post", "get"])
 def main():
@@ -10,19 +25,24 @@ def main():
 
 @app.route("/calculate", methods=["post"])
 def calculate():
-    a = request.json["a"].replace(',','.')
-    b = request.json["b"].replace(',','.')
+    a = request.json["a"].replace(',','.').replace(' ','')
+    b = request.json["b"].replace(',','.').replace(' ','')
     a = Decimal(a)
     b = Decimal(b)
+    op = request.json["op"]
     answ = 0
-    if request.json["op"] == "+":
+    if op == "+":
         answ = a+b
-    else:
+    elif op == "-":
         answ = a-b
-    return {"result": answ}
+    elif op == "*":
+        answ = a*b
+    elif op == "/":
+        answ = a/b
+    answ = answ.quantize(Decimal('.000001'), rounding=ROUND_HALF_DOWN)
+    return {"result": nicify(answ)}
 
-if __name__ == "__main__":
-  app.run(
+app.run(
     host='0.0.0.0',
     port=8080
-  )
+)
